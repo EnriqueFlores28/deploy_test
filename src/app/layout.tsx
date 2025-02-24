f2897244de3c143a25ebbb0abbadf2a039485e6d
Enriquef_28 <ef280203@gmail.com>
2025-02-24 12:53:50 -0800

    PR Corrections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ui/ScrollToTop";
+import GoHome from "@/components/ui/GoHome";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <Header />
         {children}
         <ScrollToTop />
+        <GoHome />
         <Footer />
       </body>
     </html>
--- src/app/services/warehousing/page.tsx
+++ src/app/services/warehousing/page.tsx
@@ -9,104 +9,97 @@ const images = [
   "/final/Slide31.JPG"
 ];
 
-const materials = [
+const builds = [
   {
     name: "CASAPIA WAREHOUSE 1",
-    cdate: "?/?/?",
-    location: "Tijuana, Mexico",
-    area: "?",
-    mapUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1456.848438195803!2d-116.80918783800098!3d32.46340157952854!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d947851a84432b%3A0x89af8401eb17c742!2sCasapia%20Construccion%20S.%20De%20RL.%20de%20CV.!5e0!3m2!1ses-419!2smx!4v1739558123336!5m2!1ses-419!2smx"
+    cdate: "2010",
+    location: "Tijuana, México",
+    area_m2: "13,112.12",
+    area_ft2: "141,137.69",
+    mapUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2117.145440343131!2d-116.8095213!3d32.4621853!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d93f1391c8b4b9%3A0x50f5c75d0c6e95de!2sAlmac%C3%A9n%20Casapia!5e1!3m2!1ses-419!2smx!4v1740165106391!5m2!1ses-419!2smx"
   },
   {
     name: "CASAPIA WAREHOUSE 2",
-    cdate: "?/?/?",
-    location: "Tijuana, Mexico",
-    area: "?",
-    mapUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1456.848438195803!2d-116.80918783800098!3d32.46340157952854!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d947851a84432b%3A0x89af8401eb17c742!2sCasapia%20Construccion%20S.%20De%20RL.%20de%20CV.!5e0!3m2!1ses-419!2smx!4v1739558123336!5m2!1ses-419!2smx"
+    cdate: "2021",
+    location: "Tijuana, México",
+    area_m2: "18,883.17",
+    area_ft2: "203,256.76",
+    mapUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2117.145440343131!2d-116.8095213!3d32.4621853!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d93f1391c8b4b9%3A0x50f5c75d0c6e95de!2sAlmac%C3%A9n%20Casapia!5e1!3m2!1ses-419!2smx!4v1740165106391!5m2!1ses-419!2smx"
   },
   {
     name: "CASAPIA WAREHOUSE 3",
-    cdate: "?/?/?",
-    location: "Tijuana, Mexico",
-    area: "?",
-    mapUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1456.848438195803!2d-116.80918783800098!3d32.46340157952854!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d947851a84432b%3A0x89af8401eb17c742!2sCasapia%20Construccion%20S.%20De%20RL.%20de%20CV.!5e0!3m2!1ses-419!2smx!4v1739558123336!5m2!1ses-419!2smx"
+    cdate: "2024",
+    location: "Tijuana, México",
+    area_m2: "8,877.71",
+    area_ft2: "95,558.88",
+    mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4234.4537004901795!2d-116.85691190000001!3d32.4587218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d93f00364de1d5%3A0xf23497ea72b54b02!2sCasapia%20Inzi!5e1!3m2!1ses-419!2smx!4v1740165003119!5m2!1ses-419!2smx"
   }
 ];
 
 const Warehousing = () => {
   return (
     <div className="bg-secondary min-h-screen pt-24 py-16 px-6 text-center">
-      {/* Title Section */}
       <h1 className="text-5xl mb-6 text-black">Warehousing</h1>
-
-      {/* Carousel Section */}
       <div className="mb-10">
         <Carousel images={images} />
       </div>
-
-      {/* Description Section */}
       <div className="max-w-4xl mx-auto bg-secondary mb-10 p-8 rounded-xl shadow-md">
         <p className="text-lg text-gray-600 leading-relaxed">
           Our Warehousing division offers industrial storage solutions through the rental of high-quality warehouses. Designed to meet the needs of the manufacturing and logistics sectors, our facilities provide secure, strategic, and flexible spaces that optimize operations and inventory management.
         </p>
       </div>
 
-      {/* Warehousing Table Section for Desktop */}
       <div className="max-w-5xl mx-auto overflow-x-auto hidden md:block">
         <h2 className="text-3xl font-semibold text-black mb-4">Our Available Warehousing Solutions</h2>
         <p className="text-lg text-gray-600 mb-6">
           Here you will find all the warehouses we offer to support your manufacturing and logistics needs.
         </p>
-        <table className="w-full min-w-[600px] border-collapse border border-blue-500 shadow-lg">
+        <table className="w-full min-w-[700px] border-collapse border border-blue-500 shadow-lg">
           <thead className="bg-blue-600 text-white text-lg">
             <tr>
-              <th className="border border-blue-500 px-6 py-3">Name</th>
-              <th className="border border-blue-500 px-6 py-3">Construction Date</th>
-              <th className="border border-blue-500 px-6 py-3">Location</th>
-              <th className="border border-blue-500 px-6 py-3">Building Area</th>
+              <th className="border border-blue-500 px-4 py-3">Name</th>
+              <th className="border border-blue-500 px-4 py-3">Build Date</th>
+              <th className="border border-blue-500 px-4 py-3">Location</th>
+              <th className="border border-blue-500 px-4 py-3">Area (m²)</th>
+              <th className="border border-blue-500 px-4 py-3">Area (ft²)</th>
             </tr>
           </thead>
           <tbody>
-            {materials.map((material, index) => (
+            {builds.map((builds, index) => (
               <tr key={index} className="bg-blue-100 hover:bg-blue-200 text-black">
-                <td className="border border-blue-500 px-6 py-3 font-medium">{material.name}</td>
-                <td className="border border-blue-500 px-6 py-3">{material.cdate}</td>
-                <td className="border border-blue-500 px-6 py-3">{material.location}</td>
-                <td className="border border-blue-500 px-6 py-3 font-semibold">{material.area}</td>
+                <td className="border border-blue-500 px-4 py-3 font-medium">{builds.name}</td>
+                <td className="border border-blue-500 px-4 py-3">{builds.cdate}</td>
+                <td className="border border-blue-500 px-4 py-3">{builds.location}</td>
+                <td className="border border-blue-500 px-4 py-3">{builds.area_m2} m²</td>
+                <td className="border border-blue-500 px-4 py-3">{builds.area_ft2} ft²</td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
 
-      {/* Responsive Cards for Mobile */}
       <div className="md:hidden">
         <h2 className="text-3xl font-semibold text-black mb-4">Our Available Warehousing Solutions</h2>
-        <p className="text-lg text-gray-600 mb-6">
-          Here you will find all the warehouses we offer to support your manufacturing and logistics needs.
-        </p>
-        {materials.map((material, index) => (
+        <p className="text-lg text-gray-600 mb-6">Here you will find all the warehouses we offer to support your manufacturing and logistics needs.</p>
+        {builds.map((builds, index) => (
           <div key={index} className="bg-blue-100 p-4 mb-4 rounded-lg shadow-md">
-            <h3 className="font-semibold text-lg text-black">{material.name}</h3>
-            <p className="text-gray-700">Construction Date: {material.cdate}</p>
-            <p className="text-gray-700">Location: {material.location}</p>
-            <p className="font-bold text-blue-600">Building Area: {material.area}</p>
+            <h3 className="font-semibold text-lg text-black">{builds.name}</h3>
+            <p className="text-gray-700">Build Date: {builds.cdate}</p>
+            <p className="text-gray-700">Location: {builds.location}</p>
+            <p className="text-gray-700">Area: {builds.area_m2} m² / {builds.area_ft2} ft²</p>
           </div>
         ))}
       </div>
 
-      {/* Map Iframes Section */}
       <div className="max-w-5xl mx-auto mt-10">
         <h2 className="text-3xl font-semibold text-black mb-4">Warehouse Locations</h2>
-        <p className="text-lg text-gray-600 mb-6">
-          Below are the locations of our warehouses. Click on the maps to explore.
-        </p>
-        {materials.map((material, index) => (
+        <p className="text-lg text-gray-600 mb-6">Below are the locations of our warehouses. Click on the maps to explore.</p>
+        {builds.map((build, index) => (
           <div key={index} className="mb-8">
-            <h3 className="text-xl font-semibold text-black">{material.name}</h3>
-            <p className="text-gray-700 mb-2">{material.location}</p>
+            <h3 className="text-xl font-semibold text-black">{build.name}</h3>
+            <p className="text-gray-700 mb-2">{build.location}</p>
             <iframe
-              src={material.mapUrl}
+              src={build.mapUrl}
               width="100%"
               height="300"
               style={{ border: 0 }}
